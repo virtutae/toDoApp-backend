fbbda16eb47c618dffce1a7237bb242735f29475
virtutae <virtutae@yahoo.it>
2023-09-19 23:54:07 +0100

    added the delete route

--- src/server.ts
+++ src/server.ts
@@ -39,20 +39,21 @@ app.post("/", async (req, res) => {
   }
 });
 ////////////////////////////////////
-app.delete("/:id", async (req, res) => { 
+
+app.delete("/:id", async (req, res) => {
   try {
-    const { id } = req.params;
-    await client.query(`DELETE FROM todobysilviu WHERE id = $1;`, [id]);
-    res.status(201).json({ status: "It worked" });
+    const itemId = req.params.id;
+    await client.query("DELETE FROM todobysilviu WHERE id = $1;", [itemId]);
+    res.status(204).send();
   } catch (error) {
     console.error(error);
-    res.status(500).send("An error has occured!");
-  }
-}
 
+    res.status(500).send("An error has occurred!");
+  }
+});
 
 //////////////////////
-,app.get("/health-check", async (_req, res) => {
+app.get("/health-check", async (_req, res) => {
   try {
     //For this to be successful, must connect to db
     await client.query("select now()");
@@ -62,9 +63,7 @@ app.delete("/:id", async (req, res) => {
     console.error(error);
     res.status(500).send("An error occurred. Check server logs.");
   }
-}));
-
-
+});
 
 connectToDBAndStartListening();
 
